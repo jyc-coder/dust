340da4d844395f67c5731fdfe224fdc7c394e246
jyc-coder <jyc4648@gmail.com>
2023-03-30 13:46:20 +0900

    feat: Add dustApi's endpoints (getMultipleDusts)
    - add dustApi's endpoints (getMultipleDusts) which can request multiple times with one query

--- src/apis/axios.jsx
+++ src/apis/axios.jsx
@@ -6,7 +6,6 @@ const getParameters = {
     returnType: 'json',
     numOfRows: '642',
     pageNo: '1',
-    sidoName: '시/도이름',
     ver: '1.0',
 }
 
@@ -21,15 +20,36 @@ export const dustApi = createApi({
     }),
     endpoints: (builder) => ({
         getDust: builder.query({
-            query: (sidoname) => ({
+            query: (sidoName) => ({
                 url: '',
                 params: {
                     ...getParameters,
-                    sidoName: sidoname,
+                    sidoName,
                 },
             }),
+            transformResponse: (responseData) => {
+                return responseData['response']['body']['items']
+            },
+        }),
+
+        /** 한 쿼리에서 여러번 요청을 보내고자 할때 사용하는 방식 */
+        getMultipleDusts: builder.query({
+            async queryFn(_arg, queryApi, _extraOptions, fetchWithBQ) {
+                const result = await _arg.reduce(async (promise, sidoName) => {
+                    const argResult = await fetchWithBQ({
+                        url: '',
+                        params: { ...args, sidoName: sidoName },
+                    })
+                    // 에러처리
+                    if (argResult.error) return { error: argResult.error }
+                    // 결과값을 promise에 담아서 리턴
+                    const promiseData = await promise.then()
+                    return Promise.resolve([...promiseData, ...argResult.data['response']['body']['items']])
+                }, Promise.resolve([]))
+                return result.error ? { error: result.error } : { data: result }
+            },
         }),
     }),
 })
 
-export const { useGetDustQuery } = dustApi
+export const { useGetDustQuery, useGetMultipleDustsQuery } = dustApi
